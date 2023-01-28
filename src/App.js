32070e559b1c00962da753d2fc92408e0e2c612c
Dip Singha <dipsingha2@gmail.com>
2023-01-28 20:51:49 +0600

    files added

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Career from "./components/Career";
+import Community from "./components/Community";
+import Gallery from "./components/Gallery";
+import Login from "./components/Login";
+import Magazine from "./components/Magazine";
+import Register from "./components/Register";
+import Reunion from "./components/Reunion";
+import Footer from "./components/Footer";
+import About from "./components/About";
+import NotFound from "./components/NotFound";
+
+const App = () => {
+  return (
+    <>
+      <div className="app">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/career" element={<Career />} />
+          <Route path="/community" element={<Community />} />
+          <Route path="/gallery" element={<Gallery />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/magazine" element={<Magazine />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/reunion" element={<Reunion />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default App;
---
+++ src/resource/tempData.js
@@ -0,0 +1,40 @@
+const blogsData = [
+  {
+    img: "https://www.reliablesoft.net/wp-content/uploads/2019/12/free-images-for-blogs.jpg",
+    title: "Demo title",
+    description:
+      " Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took",
+  },
+  {
+    img: "https://www.reliablesoft.net/wp-content/uploads/2019/12/free-images-for-blogs.jpg",
+    title: "Demo title",
+    description:
+      " Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took",
+  },
+  {
+    img: "https://www.reliablesoft.net/wp-content/uploads/2019/12/free-images-for-blogs.jpg",
+    title: "Demo title",
+    description:
+      " Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took",
+  },
+  {
+    img: "https://www.reliablesoft.net/wp-content/uploads/2019/12/free-images-for-blogs.jpg",
+    title: "Demo title",
+    description:
+      " Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took",
+  },
+];
+
+export default blogsData;
+// {
+//   blogsData.forEach((blog) => {
+//     console.log(blog);
+//     <div className="blog">
+//       <img src={blog.img} alt="" />
+//       <div className="details">
+//         <h2 className="title">{blog.title}</h2>
+//         <p className="description">{blog.description}</p>
+//       </div>
+//     </div>;
+//   });
+// }
